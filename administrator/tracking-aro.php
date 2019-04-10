5ba117c8f513c63a03bf5985e9748c09316b9cd3
Agie003Annan <45745519+Agie003Annan@users.noreply.github.com>
2019-04-10 16:03:30 +0700

    update

--- administrator/tracking-aro.php
+++ administrator/tracking-aro.php
@@ -67,7 +67,7 @@ if(isset($_POST['submit_col'])){
 								if($data['LEVEL'] == 'SUPER ADMIN'){
 									
 								}else{
-									$callCol = "{call SP_LOV_ARO_BY_BRANCH(?)}"; 
+									$callCol = "{call SP_GET_COLLECTOR_BY_BRANCH(?)}"; 
 									$paramsCol = array(array($data['BRANCHID'], SQLSRV_PARAM_IN));  
 									$execCol = sqlsrv_query( $conn, $callCol, $paramsCol) or die( print_r( sqlsrv_errors(), true));								
 									while($dataCol = sqlsrv_fetch_array($execCol)){
